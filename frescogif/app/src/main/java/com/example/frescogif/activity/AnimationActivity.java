df8f4826460f2677f92d84d64086fbbe303a036b
ShellRay <xiew@tuwan.com>
2021-11-23 13:28:53 +0800

    文字滚动效果

--- frescogif/app/src/main/java/com/example/frescogif/activity/AnimationActivity.java
+++ frescogif/app/src/main/java/com/example/frescogif/activity/AnimationActivity.java
@@ -7,11 +7,17 @@ import android.animation.AnimatorSet;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 
+import android.os.Handler;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.example.frescogif.R;
 import com.example.frescogif.baseActvity.BaseActivity;
+import com.example.frescogif.view.AutoTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by GG on 2017/12/14.
@@ -22,9 +28,14 @@ public class AnimationActivity extends BaseActivity implements View.OnClickListe
     private FrameLayout mainFlCardBack;
     private FrameLayout mainFlCardFront;
     private FrameLayout mainFlContainer;
+    private AutoTextView autoTextView;
+
     private AnimatorSet mRightOutSet;
     private AnimatorSet mLeftInSet;
     private boolean mIsShowBack;
+    private List<String> info;
+    private Handler handler = new Handler();
+    private boolean isFlipping = false; // 是否启用预警信息轮播
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,16 +43,73 @@ public class AnimationActivity extends BaseActivity implements View.OnClickListe
 
         setContentView(R.layout.activity_animation);
         initView();
+
     }
+    int count = 0;
 
     private void initView() {
+
+        info = new ArrayList<>();
+        info.add("心中有阳光，脚底有力量！");
+        info.add("兮兮好棒 致 小鬼头：你始终是我的唯一");
+        info.add("关关雎鸠 在河之洲");
+        info.add("窗前明月光");
+        info.add("疑似地上霜");
+        info.add("巨头邀明月");
+
         mainFlContainer = (FrameLayout) findViewById(R.id.main_fl_container);
         mainFlCardBack = (FrameLayout) findViewById(R.id.main_fl_card_back);
         mainFlCardFront = (FrameLayout) findViewById(R.id.main_fl_card_front);
+        autoTextView = (AutoTextView) findViewById(R.id.autoTextView);
 
         mainFlContainer.setOnClickListener(this);
+        autoTextView.setOnClickListener(this);
         setAnimators(); // 设置动画
         setCameraDistance(); // 设置镜头距离
+
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!isFlipping) {
+                return;
+            }
+            count++;
+            autoTextView.setText(info.get(count % info.size()));
+            if (count == info.size()) {
+                count = 0;
+            }
+            startFlipping();
+        }
+    };
+
+    //开启信息轮播
+    public void startFlipping() {
+        if (info.size() > 1) {
+            handler.removeCallbacks(runnable);
+            isFlipping = true;
+            handler.postDelayed(runnable, 3000);
+        }
+    }
+    //关闭信息轮播
+    public void stopFlipping() {
+        if (info.size() > 1) {
+            isFlipping = false;
+            handler.removeCallbacks(runnable);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startFlipping();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopFlipping();
     }
 
     @Override
@@ -51,7 +119,14 @@ public class AnimationActivity extends BaseActivity implements View.OnClickListe
             case R.id.main_fl_container:
                 flipCard(mainFlContainer);
                 break;
+            case R.id.autoTextView:
+//                autoTextView.next();
+//
+//                autoTextView.setText(info.get(count % info.size()));
+//                count ++;
+                break;
         }
+
     }
 
     // 翻转卡片
